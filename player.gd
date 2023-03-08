00186402b5bbe61ab6aee0d01a977e38047c48a3
ParadoxV5 <paradox.ver5@gmail.com>
2023-03-07 17:45:32 -0700

    `player.gd`: code movement

--- player.gd
+++ player.gd
@@ -7,3 +7,23 @@ extends Area2D
 var screen_size
 func _ready():
   screen_size = get_viewport_rect().size
+
+func _process(delta):
+  var velocity = Vector2.ZERO # The player's movement vector.
+  if Input.is_action_pressed(&"move_right"):
+    velocity.x += 1
+  if Input.is_action_pressed(&"move_left"):
+    velocity.x -= 1
+  if Input.is_action_pressed(&"move_down"):
+    velocity.y += 1
+  if Input.is_action_pressed(&"move_up"):
+    velocity.y -= 1
+  
+  if velocity.length() > 0:
+    $AnimatedSprite2D.play()
+    var position2 = position + velocity.normalized() * speed * delta
+    position2.x = clamp(position2.x, 0, screen_size.x)
+    position2.y = clamp(position2.y, 0, screen_size.y)
+    position = position2
+  else:
+    $AnimatedSprite2D.stop()
